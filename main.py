909f2b2adab70b0b5c4584ed0ec2b906402a295b
Lily Bevan <lilybevan31@gmail.com>
2025-05-06 22:09:25 +0100

    add game
    yes

---
+++ main.py
@@ -0,0 +1,12 @@
+# wordle
+
+print("guess word :33333\n is it a) inpip, b) holly or c) bovingdon")
+
+guess = input("¬")
+
+if guess.strip().lower() == "a":
+    print("wrong lol")
+elif guess.strip().lower() == "b":
+    print("mate you twonk its in the source code")
+else:
+    print("you either got it right or you didnnt type a or b, either way gg")
